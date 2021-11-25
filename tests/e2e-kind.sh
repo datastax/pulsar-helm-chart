52efc2c062a022701499cfcf5b3935622ef4b922
Lari Hotari <lhotari@users.noreply.github.com>
2021-11-25 12:53:16 +0200

    Reduce CI memory usage (#75)
    * Reduce CI memory usage
    
    * Limit MaxDirectMemorySize to 256m on bookkeeper, broker and function worker
    
    * Reduce limits even further because of 143 exit code
    
    * Add logging to loading of images to kind
    
    * Use single worker node for CI kind cluster
    
    * Use datastax/lunastreaming image for functions worker to reduce memory usage
    
    - Pulsar loads all nar files to memory after https://github.com/apache/pulsar/pull/9413
    
    * Add solution for skipping yq installation (for local development)
    
    * Bump k8s version used in kind
    
    * Add instructions how to run locally
    
    * Allow less diskspace for CI test run

--- tests/e2e-kind.sh
+++ tests/e2e-kind.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+CI="${CI:-false}"
 set -o errexit
 set -o nounset
 set -o pipefail
 
 readonly CT_VERSION=latest
 readonly KIND_VERSION=v0.11.1
-readonly K8S_VERSION=v1.18.19
+readonly K8S_VERSION=v1.21.2
 
 readonly CLUSTER_NAME=pulsar-helm-test
 
@@ -78,10 +78,12 @@ install_charts() {
 }
 
 pull_and_cache_docker_images() {
-    echo 'Installing yq...'
-    curl -Lo ./yq https://github.com/mikefarah/yq/releases/download/v4.9.8/yq_linux_amd64
-    chmod +x ./yq
-    sudo mv yq /usr/local/bin/
+    if [[ $CI == "true" ]]; then
+        echo 'Installing yq...'
+        curl -Lo ./yq https://github.com/mikefarah/yq/releases/download/v4.9.8/yq_linux_amd64
+        chmod +x ./yq
+        sudo mv yq /usr/local/bin/
+    fi
     echo 'Printing yq version'
     yq --version
 
@@ -92,7 +94,7 @@ pull_and_cache_docker_images() {
     images=$(yq e '.image | .[] |= ([.repository, .tag] | join(":")) | to_entries | .[] | .value' "$SCRIPT_DIR"/../helm-chart-sources/pulsar/values.yaml | sort | uniq)
     for image in $images; do
         docker pull "$image"
-        kind load docker-image --name "$CLUSTER_NAME" --nodes "$nodes" "$image"
+        kind load docker-image -v 1 --name "$CLUSTER_NAME" --nodes "$nodes" "$image"
     done
 }
 
