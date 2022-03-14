2b78821b2529ec3546c3f4883134d6dd7c0596b1
Enrico Olivelli <eolivelli@gmail.com>
2022-03-14 17:42:09 +0100

    Add more examples about how to run Kafka without automatic topic and namespace creation

---
+++ examples/kafka/create-tenant-full.sh
@@ -0,0 +1,21 @@
+#bin/bash
+set -e
+BASTIONPOD=$(kubectl get pods | grep bastion | awk '{print $1}' | head -n 1)
+TENANT=$1
+NAMESPACE=kafka
+NAMESPACE2=__kafka
+NAMESPACE3=__kafka_unlimited
+ROLE="$TENANT-admin"
+TOKENFILE=$TENANT.token
+CLIENTFILE=kafka.client.$TENANT.properties
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin tenants create $TENANT"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin tenants update -r $ROLE $TENANT"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces create $TENANT/$NAMESPACE"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces create $TENANT/$NAMESPACE2"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces create $TENANT/$NAMESPACE3"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces set-retention -s -1 -t -1  $TENANT/$NAMESPACE3"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar tokens create -pk token-private-key/my-private.key -s $ROLE" > $TOKENFILE
+sed s/TENANT/$TENANT/g kafka.client.properties.template | sed s/TOKEN/$(cat $TOKENFILE)/g > $CLIENTFILE
+
+
+echo "Created $CLIENTFILE with a admin token for tenant $TENANT"
