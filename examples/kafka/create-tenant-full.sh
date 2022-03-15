6baa887a16c955007089d36cef913cc56ee278b7
Enrico Olivelli <eolivelli@gmail.com>
2022-03-15 10:28:42 +0100

    Kafka Examples - Clean up scripts

--- examples/kafka/create-tenant-full.sh
+++ examples/kafka/create-tenant-full.sh
@@ -10,10 +10,21 @@ TOKENFILE=$TENANT.token
 CLIENTFILE=kafka.client.$TENANT.properties
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin tenants create $TENANT"
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin tenants update -r $ROLE $TENANT"
+
+# DATA
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces create $TENANT/$NAMESPACE"
+
+# SYSTEM TOPICS WITH RETENTION
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces create $TENANT/$NAMESPACE2"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin topics create-partitioned-topic -p 50 persistent://$TENANT/$NAMESPACE2/__consumer_offsets"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin topics create-partitioned-topic -p 8 persistent://$TENANT/$NAMESPACE2/__transaction_state"
+
+# SYSTEM TOPICS WITH UNLIMITED RETENTION
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces create $TENANT/$NAMESPACE3"
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin namespaces set-retention -s -1 -t -1  $TENANT/$NAMESPACE3"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin topics create persistent://$TENANT/$NAMESPACE3/__kafka_schemaregistry"
+kubectl exec $BASTIONPOD -- bash -c "bin/pulsar-admin topics create persistent://$TENANT/$NAMESPACE3/__kafka_producerid"
+
 kubectl exec $BASTIONPOD -- bash -c "bin/pulsar tokens create -pk token-private-key/my-private.key -s $ROLE" > $TOKENFILE
 sed s/TENANT/$TENANT/g kafka.client.properties.template | sed s/TOKEN/$(cat $TOKENFILE)/g > $CLIENTFILE
 
